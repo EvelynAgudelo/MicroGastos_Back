e87dc15437be398a69127c726c1259e3e2f70453
Sebastian <sebast.quinte13@gmail.com>
2026-03-09 08:38:51 -0500

    se crean relaciones y enums

--- NEOAPP/src/main/java/com/example/NEOAPP/modelos/Gasto.java
+++ NEOAPP/src/main/java/com/example/NEOAPP/modelos/Gasto.java
@@ -1,7 +1,83 @@
 package com.example.NEOAPP.modelos;
 
+import java.time.LocalDate;
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
 public class Gasto {
 
     //id,descripcion,fecha,valor,icono
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String descripcion;
+    private LocalDate fecha;
+    private Double valor;
+    private String icono;
+
+
+    //relacion con tabla usuario
+    //1 gasto asociado a 1 solo usuario
+    @ManyToOne
+    @JoinColumn(name = "fk_usuario", referencedColumnName = "id")
+    private Usuario usuario;
+
+    private List<Categoria>categorias;
+
+    public Gasto() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public LocalDate getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(LocalDate fecha) {
+        this.fecha = fecha;
+    }
+
+    public Double getValor() {
+        return valor;
+    }
+
+    public void setValor(Double valor) {
+        this.valor = valor;
+    }
+
+    public String getIcono() {
+        return icono;
+    }
+
+    public void setIcono(String icono) {
+        this.icono = icono;
+    }
+
+    
+
+    
 
 }
--- NEOAPP/src/main/java/com/example/NEOAPP/modelos/Usuario.java
+++ NEOAPP/src/main/java/com/example/NEOAPP/modelos/Usuario.java
@@ -1,8 +1,101 @@
 package com.example.NEOAPP.modelos;
 
+import java.util.List;
+
+import com.example.NEOAPP.modelos.utils.TipoDocumento;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="usuarios")
 public class Usuario {
 
     //id,nombres,tipodoc,documento,edad
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "nombresCompletos", nullable = false, unique = false, length = 50)
+    private String nombres;
+    
+    @Column(name = "tipo_documento", nullable = false, unique = false)
+    @Enumerated(EnumType.STRING)
+    private TipoDocumento tipoDocumento;
+    
+    @Column(name = "documento", nullable = false, unique = true, length = 15)
+    private String documento;
+    
+    @Column(name = "edad", nullable = true, unique = false)
+    private Integer edad;
+
+
+    //Creando la primera relacion con la tabla gastos
+    //1 usuario muchos gastos
+
+    @OneToMany(mappedBy = "usuario")
+    private List<Gasto> gastos;
+
+
+    public Usuario() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public TipoDocumento getTipoDocumento() {
+        return tipoDocumento;
+    }
+
+    public void setTipoDocumento(TipoDocumento tipoDocumento) {
+        this.tipoDocumento = tipoDocumento;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public Integer getEdad() {
+        return edad;
+    }
+
+    public void setEdad(Integer edad) {
+        this.edad = edad;
+    }
+
+    
+
+ 
+    
+
+    
+
+    
+
 
 
 }
