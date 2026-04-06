4f9fde8118f272c889588be6d3a37f9837d77863
Sebastian <sebast.quinte13@gmail.com>
2026-04-06 08:47:22 -0500

    ok

---
+++ NEOAPP/src/main/java/com/example/NEOAPP/servicios/UsuarioServicio.java
@@ -0,0 +1,25 @@
+package com.example.NEOAPP.servicios;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.NEOAPP.repositorios.IUsuarioRepositorio;
+
+@Service
+public class UsuarioServicio {
+
+    @Autowired
+    private IUsuarioRepositorio repositorio;
+
+    //servicio para guardar un usuario
+
+    //servicio para listar todos los usuarios en BD
+
+    //servicio para eliminar un usuario en bd 
+
+    //servicio para modificar un usuario en bd
+
+    //servicio para buscar un usuario por su id
+
+
+}
